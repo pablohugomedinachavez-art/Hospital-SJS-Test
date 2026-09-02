35c799d5815bf09586a1aa1b18a87a9d74c28dd6
pablohugomedinachavez-art <pablohugomedinachavez@gmail.com>
2026-09-02 08:38:20 -0500

    fix change patient

--- frontend/src/hospitalModules.jsx
+++ frontend/src/hospitalModules.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useState, useEffect, useEffect, useMemo, useRef, useState } from 'react'
 import {
   Bar,
   BarChart,
