b22a6a3da1122de833f040695afbd9f8a57ccab8
SIS <sis@example.com>
2026-09-04 10:15:14 -0500

    try 5 addtailwind

--- frontend/src/hospitalModules.jsx
+++ frontend/src/hospitalModules.jsx
@@ -1,4 +1,4 @@
-import { apiFetch } from './api'; // Asegúrate de que la ruta coincida con la ubicación de tu archivo api.js
+import { apiFetch } from './api.js'; // Asegúrate de que la ruta coincida con la ubicación de tu archivo api.js
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
   Bar,
